800d6085f8f44f4b512b5e363e9f641576ba8de1
Aman Yadav <amanyadav.ug.20@ctuniversity.in>
2024-01-28 17:51:38 +0530

    Few Changes in Card- Design

---
+++ card-design/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./card"
+
+function App() {
+ return(
+    <>
+    <Card/>
+    <Card/>
+    <Card/>
+    </>
+ )
+}
+
+export default App
---
+++ card-design/src/card.jsx
@@ -0,0 +1,29 @@
+
+function Card(){
+    return(
+        <><><div className="card">
+            <img className="image" src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/06154034/Akita-standing-outdoors-in-the-summer.jpg" alt="akita"></img>
+            <h1 className="card-tittle1">Dog Breed</h1>
+            <h2 className="card-tittle2">Akita</h2>
+            <p className="about">The Akita is a muscular, double-coated dog of ancient Japanese lineage famous for their dignity, courage, and loyalty. In their native land, they're venerated as family protectors and symbols of good health, happiness, and long life.
+
+                Akitas are burly, heavy-boned spitz-type dogs of imposing stature. Standing 24 to 28 inches at the shoulder, Akitas have a dense coat that comes in several colors, including white. The head is broad and massive, and is balanced in the rear by a full, curled-over tail. The erect ears and dark, shining eyes contribute to an expression of alertness, a hallmark of the breed. Akitas are quiet, fastidious dogs. Wary of strangers and often intolerant of other animals, Akitas will gladly share their silly, affectionate side with family and friends. They thrive on human companionship. The large, independent-thinking Akita is hardwired for protecting those they love. They must be well socialized from birth with people and other dogs.</p>
+        </div><div className="card">
+                <img className="image" src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/06154034/Akita-standing-outdoors-in-the-summer.jpg" alt="akita"></img>
+                <h1 className="card-tittle1">Dog Breed</h1>
+                <h2 className="card-tittle2">Akita</h2>
+                <p className="about">The Akita is a muscular, double-coated dog of ancient Japanese lineage famous for their dignity, courage, and loyalty. In their native land, they're venerated as family protectors and symbols of good health, happiness, and long life.
+
+                    Akitas are burly, heavy-boned spitz-type dogs of imposing stature. Standing 24 to 28 inches at the shoulder, Akitas have a dense coat that comes in several colors, including white. The head is broad and massive, and is balanced in the rear by a full, curled-over tail. The erect ears and dark, shining eyes contribute to an expression of alertness, a hallmark of the breed. Akitas are quiet, fastidious dogs. Wary of strangers and often intolerant of other animals, Akitas will gladly share their silly, affectionate side with family and friends. They thrive on human companionship. The large, independent-thinking Akita is hardwired for protecting those they love. They must be well socialized from birth with people and other dogs.</p>
+            </div></><div className="card">
+                <img className="image" src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/06154034/Akita-standing-outdoors-in-the-summer.jpg" alt="akita"></img>
+                <h1 className="card-tittle1">Dog Breed</h1>
+                <h2 className="card-tittle2">Akita</h2>
+                <p className="about">The Akita is a muscular, double-coated dog of ancient Japanese lineage famous for their dignity, courage, and loyalty. In their native land, they're venerated as family protectors and symbols of good health, happiness, and long life.
+
+                    Akitas are burly, heavy-boned spitz-type dogs of imposing stature. Standing 24 to 28 inches at the shoulder, Akitas have a dense coat that comes in several colors, including white. The head is broad and massive, and is balanced in the rear by a full, curled-over tail. The erect ears and dark, shining eyes contribute to an expression of alertness, a hallmark of the breed. Akitas are quiet, fastidious dogs. Wary of strangers and often intolerant of other animals, Akitas will gladly share their silly, affectionate side with family and friends. They thrive on human companionship. The large, independent-thinking Akita is hardwired for protecting those they love. They must be well socialized from birth with people and other dogs.</p>
+            </div></>
+    );
+
+}
+export default Card
